b2bcd1c6ddae468cc1256e5fdb4c5352b0ae706c
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-04-04 22:32:29 +0100

    Initial commit

---
+++ Duels/Log.cs
@@ -0,0 +1,15 @@
+﻿using BWModLoader;
+
+namespace Duels
+{
+	internal static class Log
+	{
+		// Token: 0x04000001 RID: 1
+		public static readonly ModLogger logger = new ModLogger("[DuelsGameMode]", ModLoader.LogPath + "\\DuelsGameMode.txt");
+
+		public static void log(string message)
+        {
+			logger.Log(message);
+        }
+	}
+}
---
+++ Duels/ModeHandler.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using BWModLoader;
+using UnityEngine;
+
+namespace Duels
+{
+    [Mod]
+    public class ModeHandler : MonoBehaviour
+    {
+
+        public static List<string> usersVoted = new List<string>();
+        public static bool voteSucceded = false;
+
+        public static void vote(string pName, int pSocket, bool isForced)
+        {
+            if (usersVoted.Contains(pName))
+            {
+                // To User only
+                if (isForced)
+                {
+                    DuelsGameMode.Instance.wno.òäóæåòîððòä("broadcastChat", pSocket, new object[]
+                    {
+                    1,
+                    1,
+                    "game",
+                    "You have already voted."
+                    });
+                }
+                else
+                {
+                    UI.Instance.GetComponent<WakeNetObject>().îëæêéïåðæìå("sendWarning", óëððîêðëóêó.îéäåéçèïïñí, new object[]
+                    {
+                        "all",
+                        "Switching to Duels mode..."
+                    });
+                    voteSucceded = true;
+                }
+            }
+            else
+            {
+                usersVoted.Add(pName);
+
+                // To User only
+                DuelsGameMode.Instance.wno.òäóæåòîððòä("broadcastChat", pSocket, new object[]
+                {
+                    1,
+                    1,
+                    "game",
+                    "Your vote has been counted."
+                });
+
+                // To all users
+                DuelsGameMode.Instance.wno.îëæêéïåðæìå("broadcastChat", óëððîêðëóêó.îéäåéçèïïñí, new object[]
+                {
+                    1,
+                    1,
+                    "game",
+                    string.Concat(new string[]
+                    {
+                        "User has voted for 'Duels' (",
+                        usersVoted.Count.ToString(),
+                        "/",
+                        Math.Ceiling((double)GameMode.Instance.Players.Count / 2.0).ToString(),
+                        ")"
+                    })
+                });
+
+                if ((double)usersVoted.Count >= Math.Ceiling((double)GameMode.Instance.Players.Count / 2.0) || isForced)
+                {
+                    UI.Instance.GetComponent<WakeNetObject>().îëæêéïåðæìå("sendWarning", óëððîêðëóêó.îéäåéçèïïñí, new object[]
+                    {
+                        "all",
+                        "Switching to Duels mode..."
+                    });
+                    voteSucceded = true;
+                }
+            }
+        }
+    }
+}

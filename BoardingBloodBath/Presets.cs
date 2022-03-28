07ac47c0d6cc2b12c4e76b6155cfc34f471680bd
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2022-03-28 04:37:41 +0100

    Setup presets file

---
+++ BoardingBloodBath/Presets.cs
@@ -0,0 +1,35 @@
+﻿using BoardingBloodbath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardingBloodBath
+{
+    enum Presets
+    {
+        Galleons,
+        Brigs
+    }
+    public struct Preset
+    {
+        public TeamShips navy;
+        public TeamShips pirate;
+        public Preset(ShipsHandler.Ships navyS1, ShipsHandler.Ships navyB, ShipsHandler.Ships navyS2, ShipsHandler.Ships pirateS1, ShipsHandler.Ships pirateB, ShipsHandler.Ships pirateS2)
+        {
+            navy.smallShip1 = navyS1;
+            navy.smallShip2 = navyS2;
+            navy.bigShip = navyB;
+
+            pirate.smallShip1 = pirateS1;
+            pirate.smallShip2 = pirateS2;
+            pirate.bigShip = pirateB;
+        }
+    }
+    public struct TeamShips
+    {
+        public ShipsHandler.Ships smallShip1;
+        public ShipsHandler.Ships bigShip;
+        public ShipsHandler.Ships smallShip2;
+    }
+}
